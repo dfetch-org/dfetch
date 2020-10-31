b1d6e59abd8a08a1ae94b72eb8394b0994edf80d
ben <ben.spoor@gmail.com>
2020-10-31 18:01:38 +0100

    Reformat with black

--- dfetch/resources/__init__.py
+++ dfetch/resources/__init__.py
@@ -1,7 +1,8 @@
 """Resources needed when dfetch is distributed."""
 
 import importlib.resources as pkg_resources
+
 from dfetch import resources  # pylint: disable=import-self
 
-SCHEMA_PATH = pkg_resources.path(resources, 'schema.yaml')
-TEMPLATE_PATH = pkg_resources.path(resources, 'template.yaml')
+SCHEMA_PATH = pkg_resources.path(resources, "schema.yaml")
+TEMPLATE_PATH = pkg_resources.path(resources, "template.yaml")
