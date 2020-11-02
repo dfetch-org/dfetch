dc299106a492902a64872f5fe1a3e0e5ba642edc
Ben Spoor <ben.spoor@edna.eu>
2020-11-02 23:14:54 +0100

    Fix codacy issues

--- create_venv.py
+++ create_venv.py
@@ -1,55 +1,64 @@
 #!python3.7
-"""Script to setup a venv"""
+"""Script to setup a venv."""
 
 import argparse
 import subprocess
 import venv
 
+from typing import Any
+
+
 class MyEnvBuilder(venv.EnvBuilder):
-    """
-    Replaces the BATCH script for creating a suitable venv for running the unit tests.
+    """Create a virtual environment.
+
     Conditions for use:
      + A `requirements.txt` file must exist
      By default this is relative to the current run path, but this can be specified programmatically.
     """
 
-    def __init__(self, *args, requirements=None, **kwargs):  #pylint: disable=line-too-long
+    def __init__(
+        self, *args: Any, requirements: str = "", **kwargs: Any
+    ) -> None:  # pylint: disable=line-too-long
         """
         :param requirements: Can be any path. If non, this step is skipped.
         """
         super().__init__(*args, **kwargs)
-        self.requirements = requirements or ['requirements.txt']
+        self.requirements = requirements or ["requirements.txt"]
 
-    def post_setup(self, context):
-        """
-        Hook for setting up proper env for testing.
-        """
+    def post_setup(self, context: Any) -> None:
+        """Hook for setting up proper env for testing."""
         super().post_setup(context)
 
-        print('Upgrading pip')
-        self.pip_install(context, '--upgrade', 'pip')
+        print("Upgrading pip")
+        self.pip_install(context, "--upgrade", "pip")
         for reqs in self.requirements:
-            print('Installing requirements from {}'.format(reqs))
-            self.pip_install(context, '-r', reqs)
-        print('Installing package')
-        self.pip_install(context, '-e', '.')
+            print("Installing requirements from {}".format(reqs))
+            self.pip_install(context, "-r", reqs)
+        print("Installing package")
+        self.pip_install(context, "-e", ".")
 
     @staticmethod
-    def pip_install(context, *args):
-        """
+    def pip_install(context: Any, *args: Any) -> None:
+        """Install something using pip.
+
         We run pip in isolated mode to avoid side effects from
         environment vars, the current directory and anything else
         intended for the global Python environment
         (same as EnvBuilder's _setup_pip)
         """
-        ret = subprocess.call((context.env_exe, '-Im', 'pip', 'install') + args, stderr=subprocess.STDOUT)
+        ret = subprocess.call(
+            (context.env_exe, "-Im", "pip", "install") + args,
+            stderr=subprocess.STDOUT,  # nosec
+        )
         if ret:
-            raise Exception('pip install command result was not 0 but %d' % ret)
+            raise Exception("pip install command result was not 0 but %d" % ret)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     PARSER = argparse.ArgumentParser()
-    PARSER.add_argument('-r', '--requirements', type=str, nargs='*')
+    PARSER.add_argument("-r", "--requirements", type=str, nargs="*")
     ARGS = PARSER.parse_args()
 
-    MyEnvBuilder(clear=True, with_pip=True, requirements=ARGS.requirements).create('venv')
+    MyEnvBuilder(clear=True, with_pip=True, requirements=ARGS.requirements).create(
+        "venv"
+    )
--- tests/testmanifest.py
+++ tests/testmanifest.py
@@ -1,21 +1,22 @@
-#pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
 
 import unittest
 from unittest.mock import patch, mock_open
 
-from dfetch.manifest import Manifest
+from dfetch.manifest.manifest import Manifest
 
-VERSION_ONLY = u'''
+VERSION_ONLY = u"""
 manifest:
    version: 0
-'''
+"""
 
-def given_manifest_from_text(text):
-    with patch('dfetch.manifest.open', mock_open(read_data=text)):
-        return Manifest.from_file('manifest.yaml')
+
+def given_manifest_from_text(text: str) -> Manifest:
+    with patch("dfetch.manifest.open", mock_open(read_data=text)):
+        return Manifest.from_file("manifest.yaml")
+
 
 class TestParsing(unittest.TestCase):
-
-    def test_can_read_version(self):
+    def test_can_read_version(self) -> None:
         manifest = given_manifest_from_text(VERSION_ONLY)
         self.assertEqual(manifest.version, 0)
