0e3f8cd511aa6032e3b4efc33e69370944090912
Ben Spoor <b.spoor@edna.eu>
2020-11-09 14:26:18 +0100

    Make it possible to construct Remote from either YAML or as copy

--- dfetch/manifest/remote.py
+++ dfetch/manifest/remote.py
@@ -16,17 +16,56 @@ If multiple remotes are listed ``default:`` can be explicitly specified.
           url-base: https://github.com/
 """
 
-from typing import Dict
+from typing import Dict, Optional, Union
+
+from typing_extensions import TypedDict
+
+_MandatoryRemoteDict = TypedDict("_MandatoryRemoteDict", {"name": str, "url-base": str})
+
+
+class RemoteDict(_MandatoryRemoteDict, total=False):
+    """Class representing data types of Remote class construction."""
+
+    default: Optional[bool]
 
 
 class Remote:
     """A single remote entry in the manifest file."""
 
-    def __init__(self, yamldata: Dict[str, str]) -> None:
+    def __init__(self, kwargs: RemoteDict) -> None:
         """Create the remote entry."""
-        self._name: str = yamldata["name"]
-        self._url_base: str = yamldata["url-base"]
-        self._default: bool = bool(yamldata.get("default", False))
+        self._name: str = kwargs["name"]
+        self._url_base: str = kwargs["url-base"]
+        self._default: bool = bool(kwargs.get("default", False))
+
+    @classmethod
+    def from_yaml(cls, yamldata: Union[Dict[str, str], RemoteDict]) -> "Remote":
+        """Create a remote entry in the manifest from yaml data.
+
+        Returns:
+            Remote: Entry containing the immutable remote entry
+        """
+        return cls(
+            {
+                "name": yamldata["name"],
+                "url-base": yamldata["url-base"],
+                "default": bool(yamldata.get("default", False)),
+            }
+        )
+
+    @classmethod
+    def copy(cls, other: "Remote") -> "Remote":
+        """Generate a new remote entry in the manifest from another.
+
+        Args:
+            other (Remote): Other Remote to copy the values from
+
+        Returns:
+            Remote: Entry containing the immutable remote entry
+        """
+        return cls(
+            {"name": other.name, "url-base": other.url, "default": other.is_default}
+        )
 
     @property
     def name(self) -> str:
