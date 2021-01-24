c51a913dc3aadcc167a5ed8321a914552816b308
Kris Bellemans <k.bellemans@edna.eu>
2021-01-24 21:30:12 +0100

    Add list command skeleton

--- dfetch/__main__.py
+++ dfetch/__main__.py
@@ -12,6 +12,7 @@ import dfetch.commands.environment
 import dfetch.commands.freeze
 import dfetch.commands.import_
 import dfetch.commands.init
+import dfetch.commands.list
 import dfetch.commands.update
 import dfetch.commands.validate
 import dfetch.log
@@ -42,6 +43,7 @@ def create_parser() -> argparse.ArgumentParser:
     dfetch.commands.freeze.Freeze.create_menu(subparsers)
     dfetch.commands.update.Update.create_menu(subparsers)
     dfetch.commands.validate.Validate.create_menu(subparsers)
+    dfetch.commands.list.List.create_menu(subparsers)
 
     return parser
 
