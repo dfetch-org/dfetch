3fbc9f01633a2ba7f0a9b7d592218814fc3411f9
Ben Spoor <ben.spoor@edna.eu>
2021-01-27 21:56:28 +0100

    Fixes #74: If not ssh url, also check with ls-remote if it is git

--- tests/test_git.py
+++ tests/test_git.py
@@ -7,6 +7,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
+from dfetch.manifest.project import ProjectEntry
 from dfetch.project.git import GitRepo
 from dfetch.util.cmdline import SubprocessCommandError
 
@@ -15,7 +16,7 @@ from dfetch.util.cmdline import SubprocessCommandError
     "name, cmd_result, expectation",
     [
         ("git repo", ["Yep!"], True),
-        ("not a git repo", [SubprocessCommandError("", "", "", -1)], False),
+        ("not a git repo", [SubprocessCommandError()], False),
         ("no git", [RuntimeError()], False),
     ],
 )
@@ -26,3 +27,21 @@ def test_check_path(name, cmd_result, expectation):
         run_on_cmdline_mock.side_effect = cmd_result
 
         assert GitRepo.check_path() == expectation
+
+
+@pytest.mark.parametrize(
+    "name, project, cmd_result, expectation",
+    [
+        ("SSH url", ProjectEntry({'name': "sshProject", 'url':"some.git"}), [], True),
+        ("http url", ProjectEntry({'name': "httpProject", 'url':"some/bla"}), ["Yep!"], True),
+        ("Failed command", ProjectEntry({'name': "proj1", 'url':"some/bla"}), [SubprocessCommandError()], False),
+        ("No git", ProjectEntry({'name': "proj2", 'url':"some/bla"}), [RuntimeError()], False),
+    ],
+)
+def test_check(name, project, cmd_result, expectation):
+
+    with patch("dfetch.project.git.run_on_cmdline") as run_on_cmdline_mock:
+
+        run_on_cmdline_mock.side_effect = cmd_result
+
+        assert GitRepo(project).check() == expectation
