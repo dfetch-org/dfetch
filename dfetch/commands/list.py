1d8c0ed63ce4100dd8a067b30da0359a540787d6
Kris Bellemans <k.bellemans@edna.eu>
2021-02-15 15:59:28 +0100

    Catch exception when project has not been fetched yet

--- dfetch/commands/list.py
+++ dfetch/commands/list.py
@@ -40,10 +40,13 @@ class List(dfetch.commands.command.Command):
 
         with dfetch.util.util.in_directory(os.path.dirname(path)):
             for project in projects:
-                metadata = Metadata.from_file(Metadata.from_project_entry(project).path)
                 logger.print_info_line("project", project.name)
                 logger.print_info_line("    remote", project.remote)
-                logger.print_info_line("    remote url", metadata.remote_url)
-                logger.print_info_line("    branch", metadata.branch)
-                logger.print_info_line("    last fetch", metadata.last_fetch)
-                logger.print_info_line("    revision", metadata.revision)
+                try:
+                    metadata = Metadata.from_file(Metadata.from_project_entry(project).path)
+                    logger.print_info_line("    remote url", metadata.remote_url)
+                    logger.print_info_line("    branch", metadata.branch)
+                    logger.print_info_line("    last fetch", metadata.last_fetch)
+                    logger.print_info_line("    revision", metadata.revision)
+                except FileNotFoundError:
+                    logger.print_info_line("    last fetch", "never")
