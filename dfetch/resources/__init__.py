5dda506ee94b1d952049b757d751642fb2af9812
Ben Spoor <b.spoor@edna.eu>
2021-02-20 22:36:00 +0100

    Don't confuse by rename

--- dfetch/resources/__init__.py
+++ dfetch/resources/__init__.py
@@ -1,8 +1,8 @@
 """Resources needed when dfetch is distributed."""
 
-import importlib.resources as pkg_resources
+import importlib.resources
 
 from dfetch import resources  # pylint: disable=import-self
 
-SCHEMA_PATH = pkg_resources.path(resources, "schema.yaml")
-TEMPLATE_PATH = pkg_resources.path(resources, "template.yaml")
+SCHEMA_PATH = importlib.resources.path(resources, "schema.yaml")
+TEMPLATE_PATH = importlib.resources.path(resources, "template.yaml")
