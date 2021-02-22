2edc3c1073dec545c93f5b38d90738356d0536cd
Ben Spoor <b.spoor@edna.eu>
2021-02-22 21:56:35 +0100

    Add unit tests

--- tests/test_check.py
+++ tests/test_check.py
@@ -53,3 +53,30 @@ def test_check(name, projects):
 
                 for project in projects:
                     mocked_make.return_value.check_for_update.assert_called()
+
+
+@pytest.mark.parametrize(
+    "name, projects",
+    [
+        ("empty", []),
+        ("single_project", [{"name": "my_project"}]),
+        ("two_projects", [{"name": "first"}, {"name": "second"}]),
+    ],
+)
+def test_check_child_manifests(name, projects):
+
+    parent = ProjectEntry({"name": "parent"})
+
+    with patch(
+        "dfetch.manifest.manifest.get_childmanifests"
+    ) as mocked_get_childmanifests:
+        with patch("dfetch.project.make") as mocked_make:
+
+            mocked_get_childmanifests.return_value = [
+                (mock_manifest(name, projects), "/")
+            ]
+
+            Check._check_child_manifests(parent, "somepath")
+
+            for project in projects:
+                mocked_make.return_value.check_for_update.assert_called()
