af63626936f2117cc5c5395b7aca583c674062a0
Ben Spoor <b.spoor@edna.eu>
2021-02-22 22:12:45 +0100

    Add some tests for projectentry

---
+++ tests/test_project_entry.py
@@ -0,0 +1,44 @@
+"""Test the Version object command."""
+# mypy: ignore-errors
+# flake8: noqa
+
+import pytest
+
+from dfetch.manifest.project import ProjectEntry
+
+
+def test_projectentry_name():
+    assert ProjectEntry({"name": "SomeProject"}).name == "SomeProject"
+
+
+def test_projectentry_revision():
+    assert ProjectEntry({"name": "SomeProject", "revision": "123"}).revision == "123"
+
+
+def test_projectentry_remote():
+    assert (
+        ProjectEntry({"name": "SomeProject", "remote": "SomeRemote"}).remote
+        == "SomeRemote"
+    )
+
+
+def test_projectentry_source():
+    assert ProjectEntry({"name": "SomeProject", "src": "SomePath"}).source == "SomePath"
+
+
+def test_projectentry_vcs():
+    assert ProjectEntry({"name": "SomeProject", "vcs": "git"}).vcs == "git"
+
+
+def test_projectentry_as_yaml():
+    assert ProjectEntry({"name": "SomeProject"}).as_yaml() == {
+        "name": "SomeProject",
+        "dst": "SomeProject",
+    }
+
+
+def test_projectentry_as_str():
+    assert (
+        str(ProjectEntry({"name": "SomeProject"}))
+        == "SomeProject          latest  SomeProject"
+    )
