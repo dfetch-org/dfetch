5240a1c959a3c066851bd2a118b73df4e2bdcaf2
Ben Spoor <ben.spoor@edna.eu>
2021-02-23 22:18:17 +0100

    Simplify check_quality script

--- check_quality.bat
+++ check_quality.bat
@@ -13,42 +13,8 @@ if not exist %VENV_DIR% (
 
 call .\venv\Scripts\activate.bat
 
-CALL :sub_display isort
-isort --recursive dfetch
-isort --recursive tests
-isort --recursive features
-
-CALL :sub_display black
-black dfetch tests features
-
-CALL :sub_display Pylint
-pylint dfetch --output-format=colorized
-
-CALL :sub_display flake8
-flake8 dfetch
-
-CALL :sub_display mypy
-mypy --strict dfetch
-
-CALL :sub_display doc8
-doc8 doc
-
-CALL :sub_display pydocstyle
-pydocstyle dfetch
-
-CALL :sub_display bandit
-bandit -r dfetch
-
-CALL :sub_display radon
-radon mi -nb dfetch
-radon cc -nb dfetch
+pre-commit run
 
 pause
 
 exit /b 0
-
-:sub_display
-echo.
-echo.
-echo ############################### %1 #################################
-exit /b
