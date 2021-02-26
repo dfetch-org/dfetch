6caac3be4a13bba525fa39c2c463ed5837d04fe5
Ben Spoor <b.spoor@edna.eu>
2021-02-26 20:33:48 +0100

    Also allow LICENSE with extensions

--- dfetch/project/git.py
+++ dfetch/project/git.py
@@ -192,7 +192,7 @@ class GitRepo(VCS):
         if src:
             run_on_cmdline(logger, "git config core.sparsecheckout true")
             with open(".git/info/sparse-checkout", "a") as sparse_checkout_file:
-                sparse_checkout_file.write("\n".join(["/" + src, "LICENSE", "COPYING"]))
+                sparse_checkout_file.write("\n".join(["/" + src, "LICENSE*", "COPYING*"]))
 
         run_on_cmdline(logger, f"git fetch --depth 1 origin {version}")
         run_on_cmdline(logger, "git reset --hard FETCH_HEAD")
