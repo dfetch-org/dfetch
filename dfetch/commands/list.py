1a298ec08d566ba61a6cd16ec757307e8c6cfd67
Jens Geudens <jensgeudens@hotmail.com>
2021-07-02 20:12:52 +0200

    Remove project option
    This option will be implemented later in another way.

--- dfetch/commands/list.py
+++ dfetch/commands/list.py
@@ -24,26 +24,20 @@ class List(dfetch.commands.command.Command):
     @staticmethod
     def create_menu(subparsers: "argparse._SubParsersAction") -> None:
         """Add the parser menu for this action."""
-        parser = dfetch.commands.command.Command.parser(subparsers, List)
-        parser.add_argument(
-            "--project", "-p", action="append", help="Only list selected projects"
-        )
+        dfetch.commands.command.Command.parser(subparsers, List)
 
     def __call__(self, args: argparse.Namespace) -> None:
         """Perform the list."""
         manifest, path = dfetch.manifest.manifest.get_manifest()
-        projects = (
-            [project for project in manifest.projects if project.name in args.project]
-            if args.project is not None
-            else manifest.projects
-        )
 
         with dfetch.util.util.in_directory(os.path.dirname(path)):
-            for project in projects:
+            for project in manifest.projects:
                 logger.print_info_line("project", project.name)
                 logger.print_info_line("    remote", project.remote)
                 try:
-                    metadata = Metadata.from_file(Metadata.from_project_entry(project).path)
+                    metadata = Metadata.from_file(
+                        Metadata.from_project_entry(project).path
+                    )
                     logger.print_info_line("    remote url", metadata.remote_url)
                     logger.print_info_line("    branch", metadata.branch)
                     logger.print_info_line("    last fetch", metadata.last_fetch)
