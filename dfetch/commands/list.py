174c4f860c7a089aee1d3b068dc6ba5ed6eeb466
Ben Spoor <b.spoor@edna.eu>
2021-07-16 21:40:26 +0200

    Add project argument to list

--- dfetch/commands/list.py
+++ dfetch/commands/list.py
@@ -24,14 +24,21 @@ class List(dfetch.commands.command.Command):
     @staticmethod
     def create_menu(subparsers: "argparse._SubParsersAction") -> None:
         """Add the parser menu for this action."""
-        dfetch.commands.command.Command.parser(subparsers, List)
+        parser = dfetch.commands.command.Command.parser(subparsers, List)
+        parser.add_argument(
+            "projects",
+            metavar="<project>",
+            type=str,
+            nargs="*",
+            help="Specific project(s) to list",
+        )
 
     def __call__(self, args: argparse.Namespace) -> None:
         """Perform the list."""
         manifest, path = dfetch.manifest.manifest.get_manifest()
 
         with dfetch.util.util.in_directory(os.path.dirname(path)):
-            for project in manifest.projects:
+            for project in manifest.selected_projects(args.projects):
                 logger.print_info_field("project", project.name)
                 logger.print_info_field("    remote", project.remote)
                 try:
