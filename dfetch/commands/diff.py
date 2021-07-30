9c007fd15a1ba8062c02c44019cc13ef9d4dada3
Ben Spoor <b.spoor@edna.eu>
2021-07-30 17:50:48 +0200

    Add possibility to specify revisions for diff

--- dfetch/commands/diff.py
+++ dfetch/commands/diff.py
@@ -14,6 +14,7 @@ import dfetch.project
 from dfetch.log import get_logger
 from dfetch.manifest.project import ProjectEntry
 from dfetch.project.git import GitRepo
+from dfetch.project.metadata import Metadata
 from dfetch.project.svn import SvnRepo
 from dfetch.util.util import catch_runtime_exceptions, in_directory
 
@@ -37,6 +38,15 @@ class Diff(dfetch.commands.command.Command):
             help="Don't recursively diff for child manifests.",
         )
         parser.add_argument(
+            "-r",
+            "--revs",
+            metavar="<oldrev>[:<newrev>]",
+            type=str,
+            default="",
+            help="Revision range",
+        )
+
+        parser.add_argument(
             "projects",
             metavar="<project>",
             type=str,
@@ -47,6 +57,7 @@ class Diff(dfetch.commands.command.Command):
     def __call__(self, args: argparse.Namespace) -> None:
         """Perform the diff."""
         manifest, path = dfetch.manifest.manifest.get_manifest()
+        revs = [r for r in args.revs.strip(":").split(":", maxsplit=1) if r]
 
         with in_directory(os.path.dirname(path)):
             exceptions: List[str] = []
@@ -64,7 +75,7 @@ class Diff(dfetch.commands.command.Command):
                             "You cannot generate a diff of a project that was never fetched"
                         )
                     if GitRepo.check_path(project.destination):
-                        patch = _diff_from_git(project)
+                        patch = _diff_from_git(project, revs)
                     elif SvnRepo.check_path(project.destination):
                         raise NotImplementedError("To be done!")
                     else:
@@ -75,21 +86,35 @@ class Diff(dfetch.commands.command.Command):
                     if patch:
                         logger.print_info_line(
                             project.name,
-                            f"Generating patch {patch_name} in {os.path.dirname(path)}",
+                            f"Generating patch {patch_name} from {revs[0]} to {revs[1]} in {os.path.dirname(path)}",
                         )
                         with open(patch_name, "w") as patch_file:
                             patch_file.write(patch)
                     else:
-                        logger.print_info_line(project.name, "No diffs found!")
+                        logger.print_info_line(
+                            project.name, f"No diffs found from {revs[0]} to {revs[1]}"
+                        )
 
         if exceptions:
             raise RuntimeError("\n".join(exceptions))
 
 
-def _diff_from_git(project: ProjectEntry) -> str:
+def _diff_from_git(project: ProjectEntry, revs: List[str]) -> str:
     """Generate a relative diff for a git repo."""
     repo = GitRepo(project)
-    hash_of_manifest = repo.metadata_revision()
-    current_hash = repo.current_revision()
 
-    return repo.get_diff(hash_of_manifest, current_hash)
+    if len(revs) > 2:
+        raise RuntimeError(f"Too many revisions given! {revs}")
+
+    if not revs:
+        revs.append(repo.metadata_revision())
+        if not revs[-1]:
+            raise RuntimeError(
+                f"When not providing any commits, dfetch starts from the last commit to {Metadata.FILENAME} in {project.destination}",
+                "Please either commit this, or specify a revision to start from with --revs",
+            )
+
+    if len(revs) == 1:
+        revs.append(repo.current_revision())
+
+    return repo.get_diff(*revs)
