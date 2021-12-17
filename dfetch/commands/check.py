07fe67953d82c0cf6f9861eb152420740b0e1fb1
Ben Spoor <b.spoor@edna.eu>
2021-12-17 22:59:21 +0100

    Check child manifests just after update

--- dfetch/commands/check.py
+++ dfetch/commands/check.py
@@ -21,14 +21,12 @@ import argparse
 import os
 from typing import List
 
-import yaml
-
 import dfetch.commands.command
 import dfetch.manifest.manifest
 import dfetch.manifest.validate
 import dfetch.project
+from dfetch.commands.common import check_child_manifests
 from dfetch.log import get_logger
-from dfetch.manifest.project import ProjectEntry
 from dfetch.util.util import catch_runtime_exceptions, in_directory
 
 logger = get_logger(__name__)
@@ -70,53 +68,7 @@ class Check(dfetch.commands.command.Command):
 
                 if not args.no_recommendations and os.path.isdir(project.destination):
                     with in_directory(project.destination):
-                        exceptions += Check._check_child_manifests(
-                            manifest, project, path
-                        )
+                        check_child_manifests(manifest, project, path)
 
         if exceptions:
             raise RuntimeError("\n".join(exceptions))
-
-    @staticmethod
-    def _check_child_manifests(
-        manifest: dfetch.manifest.manifest.Manifest, project: ProjectEntry, path: str
-    ) -> List[str]:
-        exceptions: List[str] = []
-
-        recommendations: List[ProjectEntry] = []
-        for (
-            childmanifest,
-            childmanifest_path,
-        ) in dfetch.manifest.manifest.get_childmanifests(skip=[path]):
-            for childproject in childmanifest.projects:
-
-                if childproject.remote_url not in [
-                    project.remote_url for project in manifest.projects
-                ]:
-                    recommendations.append(childproject.as_recommendation())
-
-            if recommendations:
-                rel_path = os.path.relpath(
-                    childmanifest_path, start=os.path.dirname(path)
-                ).replace("\\", "/")
-                logger.warning(
-                    "\n".join(
-                        [
-                            "",
-                            f'"{project.name}" depends on the following project(s) '
-                            "which are not part of your manifest:",
-                            f"(found in {rel_path})",
-                        ]
-                    )
-                )
-
-                recommendation_json = yaml.dump(
-                    [proj.as_yaml() for proj in recommendations],
-                    indent=4,
-                    sort_keys=False,
-                )
-                logger.warning("")
-                for line in recommendation_json.splitlines():
-                    logger.warning(line)
-
-        return exceptions
