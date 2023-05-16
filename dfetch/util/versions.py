5039392c05bea95c2d6e1d4dd82d9b21ac3373d9
Ben <ben.spoor@gmail.com>
2023-05-16 14:58:07 +0000

    Use semver3 Verion instead of VersionInfo

--- dfetch/util/versions.py
+++ dfetch/util/versions.py
@@ -2,9 +2,9 @@
 
 import re
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
-from semver import VersionInfo
+from semver.version import Version
 
 BASEVERSION = re.compile(
     r"""[vV]?
@@ -20,7 +20,7 @@ BASEVERSION = re.compile(
 )
 
 
-def coerce(version: str) -> Tuple[str, VersionInfo, str]:
+def coerce(version: str) -> Tuple[str, Optional[Version], str]:
     """Convert an incomplete version string into a semver-compatible Version object.
 
     * Tries to detect a "basic" version string (``major.minor.patch``).
@@ -38,29 +38,33 @@ def coerce(version: str) -> Tuple[str, VersionInfo, str]:
         return ("", None, version)
 
     ver = {
-        key: 0 if value is None else value for key, value in match.groupdict().items()
+        key: 0 if value is None else int(value)
+        for key, value in match.groupdict().items()
     }
 
     return (
         match.string[: match.start()],
-        VersionInfo(**ver),
+        Version(**ver),
         match.string[match.end() :],  # noqa:E203
     )
 
 
 def latest_tag_from_list(current_tag: str, available_tags: List[str]) -> str:
     """Based on the given tag string and list of tags, get the latest available."""
-    parsed_tags: Dict[str, List[Tuple[VersionInfo, str]]] = defaultdict(list)
+    parsed_tags: Dict[str, List[Tuple[Version, str]]] = defaultdict(list)
     for available_tag in available_tags:
         prefix, version, _ = coerce(available_tag)
         if version:
             parsed_tags[prefix] += [(version, available_tag)]
 
-    prefix, version, _ = coerce(current_tag)
+    prefix, current_version, _ = coerce(current_tag)
 
-    available_tag, available_string = version, current_tag
-    for tag in parsed_tags[prefix]:
-        if tag[0] > available_tag:
-            available_tag, available_string = tag
+    latest_string: str = current_tag
 
-    return available_string
+    if current_version:
+        latest_tag: Version = current_version
+        for tag in parsed_tags[prefix]:
+            if tag[0] > latest_tag:
+                latest_tag, latest_string = tag
+
+    return latest_string
--- tests/test_util_version.py
+++ tests/test_util_version.py
@@ -3,7 +3,7 @@
 # flake8: noqa
 
 import pytest
-from semver import VersionInfo
+from semver.version import Version
 
 import dfetch.util.versions
 
@@ -14,58 +14,58 @@ import dfetch.util.versions
         (
             "prefix",
             "module/1.2.3",
-            VersionInfo(major=1, minor=2, patch=3),
+            Version(major=1, minor=2, patch=3),
             "module/",
             "",
         ),
         (
             "prefix-lower-v",
             "module/v1.2.3",
-            VersionInfo(major=1, minor=2, patch=3),
+            Version(major=1, minor=2, patch=3),
             "module/",
             "",
         ),
         (
             "prefix-upper-v",
             "module/V1.2.3",
-            VersionInfo(major=1, minor=2, patch=3),
+            Version(major=1, minor=2, patch=3),
             "module/",
             "",
         ),
         (
             "prefix-multi-v",
             "vvV1.2.3",
-            VersionInfo(major=1, minor=2, patch=3),
+            Version(major=1, minor=2, patch=3),
             "vv",
             "",
         ),
         (
             "prefix-multi-versions",
             "v1.2.3-v2.3.4",
-            VersionInfo(major=1, minor=2, patch=3),
+            Version(major=1, minor=2, patch=3),
             "",
             "-v2.3.4",
         ),
-        ("empty", "", VersionInfo(major=0, minor=0, patch=0), "", ""),
-        ("only-major-minor", "1.2", VersionInfo(major=1, minor=2, patch=0), "", ""),
+        ("empty", "", Version(major=0, minor=0, patch=0), "", ""),
+        ("only-major-minor", "1.2", Version(major=1, minor=2, patch=0), "", ""),
         (
             "only-major-minor-lower-v",
             "v1.2",
-            VersionInfo(major=1, minor=2, patch=0),
+            Version(major=1, minor=2, patch=0),
             "",
             "",
         ),
         (
             "only-major-minor-upper-v",
             "V1.2",
-            VersionInfo(major=1, minor=2, patch=0),
+            Version(major=1, minor=2, patch=0),
             "",
             "",
         ),
-        ("only-major-lower-v", "v1", VersionInfo(major=1, minor=0, patch=0), "", ""),
-        ("only-major-upper-v", "V1", VersionInfo(major=1, minor=0, patch=0), "", ""),
-        ("only-major", "1", VersionInfo(major=1, minor=0, patch=0), "", ""),
-        ("only-major", "1", VersionInfo(major=1, minor=0, patch=0), "", ""),
+        ("only-major-lower-v", "v1", Version(major=1, minor=0, patch=0), "", ""),
+        ("only-major-upper-v", "V1", Version(major=1, minor=0, patch=0), "", ""),
+        ("only-major", "1", Version(major=1, minor=0, patch=0), "", ""),
+        ("only-major", "1", Version(major=1, minor=0, patch=0), "", ""),
     ],
 )
 def test_version_parsing(
@@ -78,3 +78,46 @@ def test_version_parsing(
 
     if tag_string:
         assert expected_version == version
+
+
+@pytest.mark.parametrize(
+    "name, current_tag, available_tags, expected_result",
+    [
+        ("single-available-tags", "v1.0.0", ["v1.0.0"], "v1.0.0"),
+        (
+            "multiple-available-tags-diff-versions",
+            "v2.0.0",
+            ["v1.0.0", "v2.0.0", "v3.0.0"],
+            "v3.0.0",
+        ),
+        (
+            "multiple-available-tags-same-versions",
+            "v1.0.0",
+            ["v1.0.0", "v1.0.0-alpha", "v1.0.0-beta"],
+            "v1.0.0",
+        ),
+        (
+            "multiple-available-tags-diff-prefix",
+            "bla/v1.0.0",
+            ["bla/v1.0.0", "ignore/v1.0.1", "bla/v0.9.0"],
+            "bla/v1.0.0",
+        ),
+        (
+            "no-available-tags",
+            "v1.0.0",
+            [],
+            "v1.0.0",
+        ),
+        (
+            "without-version",
+            "no-version",
+            ["no-version", "v1.0.0", "v2.0.0"],
+            "no-version",
+        ),
+    ],
+)
+def test_latest_tag_from_list(name, current_tag, available_tags, expected_result):
+    assert (
+        dfetch.util.versions.latest_tag_from_list(current_tag, available_tags)
+        == expected_result
+    )
