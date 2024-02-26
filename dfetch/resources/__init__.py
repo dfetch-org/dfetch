ae323192747fc9cf8f6662e28968485622bd0e97
Ben <ben.spoor@gmail.com>
2024-02-26 20:54:46 +0000

    Use as_file instead of deprecated path

--- dfetch/resources/__init__.py
+++ dfetch/resources/__init__.py
@@ -5,15 +5,27 @@ try:
 except ModuleNotFoundError:
     import importlib_resources  # type:ignore
 
+import sys
 from pathlib import Path
 from typing import ContextManager
 
 from dfetch import resources  # pylint: disable=import-self
 
 
+def _resource_path(filename: str) -> ContextManager[Path]:
+    """Get the path to the resource."""
+    if sys.version_info >= (3, 9):
+        return importlib_resources.as_file(
+            importlib_resources.files(resources) / filename
+        )
+    return importlib_resources.path(  # pylint: disable=deprecated-method
+        resources, filename
+    )
+
+
 def schema_path() -> ContextManager[Path]:
     """Get path to schema."""
-    return importlib_resources.path(resources, "schema.yaml")
+    return _resource_path("schema.yaml")
 
 
-TEMPLATE_PATH = importlib_resources.path(resources, "template.yaml")
+TEMPLATE_PATH = _resource_path("template.yaml")
