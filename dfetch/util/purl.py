e2ab7d5c83fff5d1bef1ea299c1afdd2faf90704
Ben <ben.spoor@gmail.com>
2025-09-15 19:29:54 +0000

    Simplify code

--- dfetch/util/purl.py
+++ dfetch/util/purl.py
@@ -4,17 +4,16 @@ Supports: GitHub, Bitbucket, SVN, SSH paths, and more.
 """
 
 import re
-from typing import Callable, List, Optional
+from typing import List, Optional, Tuple
 from urllib.parse import urlparse
 
 from packageurl import PackageURL
 from tldextract import TLDExtract
 
-HandlerType = Callable[[str, str, Optional[str], Optional[str]], Optional[PackageURL]]
-
 NO_FETCH_EXTRACT = TLDExtract(suffix_list_urls=())
 SSH_REGEX = re.compile(
-    r"^(?:git@|git\+ssh://git@)(?P<host>[^/:]+)[:/](?P<path>.+?)(?:\.git)?$"
+    r"^(?:git@|git\+ssh://git@)(?P<host>[^/:]+)[:/](?P<path>.+?)(?:\.git)?$",
+    re.IGNORECASE,
 )
 
 GITHUB_REGEX = re.compile(
@@ -34,128 +33,17 @@ EXCLUDED_DOMAINS = ["gitlab", "gitea", "gitee"]
 DEFAULT_NAME = "unknown"
 
 
-def _handle_github(
-    remote_url: str,
-    _: str,
-    version: Optional[str],
-    subpath: Optional[str],
-) -> Optional[PackageURL]:
-    """Handler for GitHub URLs."""
-    match = GITHUB_REGEX.match(remote_url)
-    if match:
-        return PackageURL(
-            type="github",
-            namespace=match.group("org"),
-            name=match.group("repo"),
-            version=version,
-            subpath=subpath,
-        )
-    return None
-
-
-def _handle_bitbucket(
-    remote_url: str,
-    _: str,
-    version: Optional[str],
-    subpath: Optional[str],
-) -> Optional[PackageURL]:
-    """Handler for Bitbucket URLs."""
-    match = BITBUCKET_REGEX.match(remote_url)
-    if match:
-        return PackageURL(
-            type="bitbucket",
-            namespace=match.group("org"),
-            name=match.group("repo"),
-            version=version,
-            subpath=subpath,
-        )
-    return None
-
-
-def _handle_svn(
-    remote_url: str,
-    path: str,
-    version: Optional[str],
-    subpath: Optional[str],
-) -> Optional[PackageURL]:
-    """Handler for SVN URLs."""
-    parsed = urlparse(remote_url)
-    if "svn" in parsed.scheme or "svn." in parsed.netloc:
-        domain = NO_FETCH_EXTRACT(parsed.netloc).domain
-
-        parts: list[str] = [domain]
-        if path:
-            parts.extend(path.split("/"))
-        name = parts[-1] if parts else DEFAULT_NAME
-        namespace = "/".join(parts[:-1])
-
-        return PackageURL(
-            type="generic",
-            namespace=namespace,
-            name=name,
-            version=version,
-            qualifiers={"vcs_url": remote_url},
-            subpath=subpath,
-        )
-    return None
-
-
-def _handle_ssh(
-    remote_url: str,
-    path: str,
-    version: Optional[str],
-    subpath: Optional[str],
-) -> Optional[PackageURL]:
-    """Handler for SSH URLs."""
-    match = SSH_REGEX.match(remote_url)
-    if match:
-        domain = NO_FETCH_EXTRACT(match.group("host")).domain
-
-        parts: list[str] = []
-        if domain not in EXCLUDED_DOMAINS:
-            parts.append(domain)
-
-        path = match.group("path")
-        if path:
-            parts.extend(path.replace(".git", "").split("/"))
-        name = parts[-1] if parts else DEFAULT_NAME
-        namespace = "/".join(parts[:-1])
-        return PackageURL(
-            type="generic",
-            namespace=namespace,
-            name=name,
-            version=version,
-            qualifiers={"vcs_url": remote_url},
-            subpath=subpath,
-        )
-    return None
-
-
-def _handle_generic(
-    remote_url: str,
-    path: str,
-    version: Optional[str],
-    subpath: Optional[str],
-) -> PackageURL:
-    """Fallback handler for generic URLs."""
-    domain = NO_FETCH_EXTRACT(remote_url).domain
-
-    parts: list[str] = []
-    if domain not in EXCLUDED_DOMAINS:
-        parts.append(domain)
+def _name_and_namespace_from_domain_and_path(domain: str, path: str) -> Tuple[str, str]:
+    """Split the full path to a name and namespace."""
+    domain = NO_FETCH_EXTRACT(domain).domain
+    parts: List[str] = [domain] if domain not in EXCLUDED_DOMAINS else []
 
     if path:
-        parts.extend(path.replace(".git", "").split("/"))
+        parts.extend(path.split("/"))
     name = parts[-1] if parts else DEFAULT_NAME
     namespace = "/".join(parts[:-1])
-    return PackageURL(
-        type="generic",
-        namespace=namespace,
-        name=name,
-        version=version,
-        qualifiers={"vcs_url": remote_url},
-        subpath=subpath,
-    )
+
+    return name, namespace
 
 
 def remote_url_to_purl(
@@ -166,18 +54,49 @@ def remote_url_to_purl(
     Supports GitHub, Bitbucket, SVN, SSH paths.
     Optionally specify version and subpath.
     """
+    match = GITHUB_REGEX.match(remote_url)
+    if match:
+        return PackageURL(
+            type="github",
+            namespace=match.group("org"),
+            name=match.group("repo"),
+            version=version,
+            subpath=subpath,
+        )
+
+    match = BITBUCKET_REGEX.match(remote_url)
+    if match:
+        return PackageURL(
+            type="bitbucket",
+            namespace=match.group("org"),
+            name=match.group("repo"),
+            version=version,
+            subpath=subpath,
+        )
+
     parsed = urlparse(remote_url)
     path = parsed.path.lstrip("/")
 
-    handlers: List[HandlerType] = [
-        _handle_github,
-        _handle_bitbucket,
-        _handle_svn,
-        _handle_ssh,
-    ]
-    for handler in handlers:
-        result: Optional[PackageURL] = handler(remote_url, path, version, subpath)
-        if result is not None:
-            return result
+    if "svn" in parsed.scheme or "svn." in parsed.netloc:
+        name, namespace = _name_and_namespace_from_domain_and_path(parsed.netloc, path)
 
-    return _handle_generic(remote_url, path, version, subpath)
+    else:
+        match = SSH_REGEX.match(remote_url)
+        if match:
+            name, namespace = _name_and_namespace_from_domain_and_path(
+                match.group("host"),
+                match.group("path"),
+            )
+        else:
+            name, namespace = _name_and_namespace_from_domain_and_path(
+                remote_url, path.replace(".git", "")
+            )
+
+    return PackageURL(
+        type="generic",
+        namespace=namespace,
+        name=name,
+        version=version,
+        qualifiers={"vcs_url": remote_url},
+        subpath=subpath,
+    )
