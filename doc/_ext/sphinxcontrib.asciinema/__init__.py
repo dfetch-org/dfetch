f75e8ab8f235129d8fc4e2ee9233aefa876ece6f
Ben <ben.spoor@gmail.com>
2025-10-14 21:12:11 +0000

    dfetch sphinxcontrib.asciinema into project

---
+++ doc/_ext/sphinxcontrib.asciinema/__init__.py
@@ -0,0 +1,23 @@
+__copyright__ = 'Copyright (C) 2023'
+__license__ = 'MIT'
+__version__ = "0.4.2"
+
+
+def setup(app):
+    from .asciinema import Asciinema, ASCIINemaDirective
+    from .asciinema import copy_asset_files, _NODE_VISITORS
+
+    app.add_config_value('sphinxcontrib_asciinema_defaults', {}, 'html')
+
+    app.connect('build-finished', copy_asset_files)
+    app.add_js_file('asciinema-player_3.6.3.js')
+    app.add_css_file('asciinema-player_3.6.3.css')
+
+    app.add_node(Asciinema, **_NODE_VISITORS)
+    app.add_directive('asciinema', ASCIINemaDirective)
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
