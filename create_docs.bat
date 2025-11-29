80689a00320c6542450dd270da129f25373ee57b
Ben Spoor <b.spoor@edna.eu>
2025-11-29 12:00:35 +0100

    Move create_venv.py to script folder and make it more robust.

--- create_docs.bat
+++ create_docs.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 cd %~dp0
 
-python create_venv.py --extra_requirements "docs"
+py script/create_venv.py --extra_requirements "docs"
 if not !ERRORLEVEL! == 0 echo "Something went wrong creating the venv." && exit /b !ERRORLEVEL!
 
 call .\venv\Scripts\activate.bat
--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 cd %~dp0..
 
-python create_venv.py --extra_requirements "test"
+py script/create_venv.py --extra_requirements "test"
 if not !ERRORLEVEL! == 0 echo "Something went wrong creating the venv." && exit /b !ERRORLEVEL!
 
 call .\venv\Scripts\activate.bat
