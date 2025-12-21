5c4cb52cea1263e8389991f18fbf98c38abbe0c4
Ben <ben.spoor@gmail.com>
2025-12-21 11:29:59 +0000

    Latest master

--- doc/_ext/sphinxcontrib_asciinema/__init__.py
+++ doc/_ext/sphinxcontrib_asciinema/__init__.py
@@ -1,6 +1,6 @@
 __copyright__ = 'Copyright (C) 2023'
 __license__ = 'MIT'
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 
 
 def setup(app):
@@ -10,8 +10,8 @@ def setup(app):
     app.add_config_value('sphinxcontrib_asciinema_defaults', {}, 'html')
 
     app.connect('build-finished', copy_asset_files)
-    app.add_js_file("asciinema-player_3.12.1.js")
-    app.add_css_file("asciinema-player_3.12.1.css")
+    app.add_js_file('asciinema-player_3.12.1.js')
+    app.add_css_file('asciinema-player_3.12.1.css')
 
     app.add_node(Asciinema, **_NODE_VISITORS)
     app.add_directive('asciinema', ASCIINemaDirective)
